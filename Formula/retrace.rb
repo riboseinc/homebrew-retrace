fddcc84c46f310d00116f3a950633ec01480d7ee
Jin JinRu <jin.jinru840430@gmail.com>
2017-12-19 17:45:54 +0800

    Added release 1.0.0

--- Formula/retrace.rb
+++ Formula/retrace.rb
@@ -2,6 +2,8 @@ class Retrace < Formula
   desc "Versatile Security Vulnerability / Bug Discovery Tool"
   homepage "https://github.com/riboseinc/retrace"
   head "https://github.com/riboseinc/retrace.git"
+  url "https://github.com/riboseinc/retrace/archive/v1.0.0.tar.gz"
+  sha256 "5a8b0619852a72421cbb37a0c3f68187dab2883090834f3adbc58413519d3f99"
   
   depends_on "autoconf" => :build
   depends_on "automake" => :build
@@ -10,7 +12,7 @@ class Retrace < Formula
   depends_on "openssl"
 
   devel do
-    version '0.9.0'
+    version '1.0.0'
   end
 
   def install
